0d4f0e40ae6bfd65f08725801a6b16795e62c539
Behailu Shiferaw <behailushiferaw@gmail.com>
2011-10-04 09:07:30 +0300

    OrderReceived Action set to send the email

--- OrderManagement/Controllers/OrderController.cs
+++ OrderManagement/Controllers/OrderController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OrderManagement.Web.Helper;
+using OrderManagement.Web.Infrastructure;
+using OrderManagment.Core.Entities;
+using Raven.Client.Document;
 
 namespace OrderManagement.Web.Controllers
 {
@@ -16,5 +20,23 @@ namespace OrderManagement.Web.Controllers
             return View();
         }
 
+        public ActionResult OrderReceived(int id)
+        {
+            var order = new Order();
+            using(var session=DocumentStoreHolder.DocumentStore.OpenSession())
+            {
+                order = session.Load<Order>(id);
+                if (order == null)
+                    ViewData["Message"] = "Incorrect Order Id";
+                else
+                {
+                    if (EmailSender.SendEMail(order))
+                        ViewData["Message"] = "Your licences have been sent to the email address provided. We thank you.";
+                    else
+                        ViewData["Message"] = "There was an error sending an email, we will contact you soon.";
+                }
+            }
+            return View();
+        }
     }
 }
--- OrderManagement/Helper/EmailSender.cs
+++ OrderManagement/Helper/EmailSender.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
+using OrderManagment.Core.Entities;
 
 namespace OrderManagement.Web.Helper
 {
@@ -11,29 +12,43 @@ namespace OrderManagement.Web.Helper
     {
         //Read SMTP configuration from web.config
         //should be with attachment
-        public static bool SendEMail(string recepient)
+        public static bool SendEMail(Order order)
         {
             var customerServiceEmail = ConfigurationManager.AppSettings["ReturnAddress"];
-            var mailMessage = new MailMessage()
-                                  {
-                                      
-                                  };
-
-            if(customerServiceEmail!=null)
-                mailMessage.ReplyToList.Add(new MailAddress(customerServiceEmail));
-
-            try
+            foreach (var orderItem in order.OrderItems)
             {
-                using (var smtpClient = new SmtpClient())
+                string message = "Dear " + order.Customer.FullName + "\n" + //Construct the appropriate message here
+                                 "Attached is your licence for the product " + orderItem.Product.ProductName;
+                //This is gives the user to confirm the receipt of the licence file
+                message += "Please click the following link to confirm receipt of the licence http://localhost:/Order/Confirmation/";
+
+                string subject = "Licence from Hibernating Rhino";
+                var mailMessage = new MailMessage()
+                                      {
+                                          IsBodyHtml = true,
+                                          Body = message,
+                                          Subject = subject
+                                      };
+                mailMessage.To.Add(new MailAddress(order.Customer.Email));
+                
+                if(customerServiceEmail!=null)
+                    mailMessage.ReplyToList.Add(new MailAddress(customerServiceEmail));
+
+                try
+                {  
+                    var attachment = new Attachment("http://localhost/Licence/Create");//the path to the generated licence file goes here.
+                    mailMessage.Attachments.Add(attachment);
+                    using (var smtpClient = new SmtpClient())
+                    {
+                        smtpClient.Send(mailMessage);
+                    }
+                }
+                catch
                 {
-                    smtpClient.Send(mailMessage);
-                    return true;
+                    return false;
                 }
             }
-            catch
-            {
-                return false;
-            }
+            return true;
         }
     }
 }
